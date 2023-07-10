105642221400812e4fa14596694716805d1beaea
Larry Johnson <deshae.johnson09@gmail.com>
2023-07-10 15:48:51 -0500

    finished project

--- week20_project/variables.tf
+++ week20_project/variables.tf
@@ -1,15 +1,32 @@
+# Specify AWS Region
 variable "aws_region" {
   type    = string
-  default = "us-east-2"
+  default = "us-east-1"
 }
 
+# Instance Type
 variable "instance_type" {
   type    = string
   default = "t2.micro"
 }
 
-variable "ami" {
+# Instance ID
+variable "ami_id" {
   description = "AMI ID of instance"
   type        = string
-  default     = "ami-053b0d53c279acc900"
+  default     = "ami-053b0d53c279acc90"
 }
+
+# Configure your VPC 
+variable "default_vpc_id" {
+  description = "ID of your VPC"
+  default     = "vpc-0c4942518add2b1ee"
+
+}
+
+# Security group name
+variable "jenkins_sg" {
+  description = "Jenkins_Security"
+  default     = "jenkins_terraform"
+}
+
